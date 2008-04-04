fe080d21f2979aa007d09481f7dab7d085d7d2e9
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2008-04-04 13:30:09 +0000

    Changed WLM poll interval to use millisecond resolution instead of second resolution.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@135 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- client/dnxWLM.c
+++ client/dnxWLM.c
@@ -564,8 +564,8 @@ static void * dnxWlm(void * data)
       DNX_PT_MUTEX_LOCK(&iwlm->mutex);
 
       gettimeofday(&now, 0);
-      timeout.tv_sec = now.tv_sec + iwlm->cfg.pollInterval;
-      timeout.tv_nsec = now.tv_usec * 1000;
+      timeout.tv_sec = now.tv_sec + (iwlm->cfg.pollInterval / 1000);
+      timeout.tv_nsec = (now.tv_usec + (iwlm->cfg.pollInterval % 1000) * 1000L) * 1000L;
 
       pthread_cond_timedwait(&iwlm->cond, &iwlm->mutex, &timeout);
 
