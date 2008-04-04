6e1a813c12de8c61df620a2248c41b51c2250dd4
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2008-04-04 13:27:18 +0000

    Remove old copyright headers, change original copyright holder to author.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@52 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- common/dnxTransport.c
+++ common/dnxTransport.c
@@ -159,26 +159,10 @@
 //
 //    GUID, State, Execution Start Time, Execution End Time, Result Code, Result Data
 //
-// Copyright (c) 2006-2007 Robert W. Ingraham (dnx-devel@lists.sourceforge.net)
+// Author: Robert W. Ingraham (dnx-devel@lists.sourceforge.net)
 //
 // First Written:   2006-06-19
 // Last Modified:   2007-09-25
-//
-// License:
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 2 as
-// published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//
 
 
 #include <stdio.h>
--- server/dnxCollector.c
+++ server/dnxCollector.c
@@ -27,26 +27,10 @@
 // check from the Jobs queue and posts the result to the existing
 // Nagios service_result_buffer.
 //
-// Copyright (c) 2006-2007 Robert W. Ingraham (dnx-devel@lists.sourceforge.net)
+// Author: Robert W. Ingraham (dnx-devel@lists.sourceforge.net)
 //
 // First Written:   2006-07-11
 // Last Modified:   2007-08-16
-//
-// License:
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License version 2 as
-// published by the Free Software Foundation.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//
 
 
 #include "dnxNebMain.h"
