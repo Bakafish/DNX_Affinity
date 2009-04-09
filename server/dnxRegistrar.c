326998828e6c098a1416cb09b210b64ec2d40e12
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2009-04-09 11:00:25 +0000

    Find the missing checks.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@392 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxRegistrar.c
+++ server/dnxRegistrar.c
@@ -252,7 +252,7 @@ int dnxGetNodeRequest(DnxRegistrar * reg, DnxNodeRequest ** ppNode, unsigned lon
 
    assert(reg && ppNode);
 
-dnxDebug(4, "dnxGetNodeRequest: Entering loop");
+dnxDebug(4, "dnxGetNodeRequest: Entering loop (%i)", ireg->tid);
 
    while ((ret = dnxQueueGet(ireg->rqueue, (void **)&node)) == DNX_OK)
    {
