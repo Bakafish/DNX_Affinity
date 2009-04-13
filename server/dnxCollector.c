7c53532c391183a51cc8621d1a34b2519696906b
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2009-04-13 09:36:19 +0000

    Find the missing checks.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@436 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxCollector.c
+++ server/dnxCollector.c
@@ -117,12 +117,12 @@ static void * dnxCollector(void * data)
                 srv = (nebstruct_service_check_data *)Job.check_data;
                 svc_description = xstrdup(srv->service_description);
                 host_name = xstrdup(srv->host_name);
-                dnxDebug(4, "dnxCollector: Hostname (%s)", host_name);
+                dnxDebug(4, "dnxCollector: Service Check Hostname (%s)(%s)", host_name, srv->host_name);
             } else {
                 dnxDebug(4, "dnxCollector: Host Check (%i)", Job.check_data);
                 hst = (nebstruct_host_check_data *)Job.check_data;
                 host_name = xstrdup(hst->host_name);
-                dnxDebug(4, "dnxCollector: Hostname (%s)", host_name);
+                dnxDebug(4, "dnxCollector: Host Check Hostname (%s)(%s)", host_name, hst->host_name);
             }
             time_t check_time = Job.start_time + sResult.delta;
             dnxDebug(2, "dnxCollector[%lx]: Hostname(%s) Service(%s)", 
