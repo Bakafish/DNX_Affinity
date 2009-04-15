5374c852bc36abde74cc36432f41bbff55c72a61
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2009-04-15 07:46:40 +0000

    Bug fix.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@454 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxNebMain.c
+++ server/dnxNebMain.c
@@ -969,8 +969,8 @@ static int ehHstCheck(int event_type, void * data)
 
 	/* set check time for on-demand checks, so they're not incorrectly detected as being orphaned - Luke Ross 5/16/08 */
 	/* NOTE: 06/23/08 EG not sure if there will be side effects to this or not.... */
-	if(scheduled_check==FALSE)
-		hostObj->next_check=hstdata->start_time.tv_sec;
+// 	if(scheduled_check==FALSE)
+// 		hostObj->next_check=hstdata->start_time.tv_sec;
 
 	/* increment number of host checks that are currently running... */
 	extern currently_running_host_checks++;
