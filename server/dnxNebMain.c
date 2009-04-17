618a94362568b27d7728db3322286cf44ec938a0
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2009-04-17 01:40:35 +0000

    Fix.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@506 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxNebMain.c
+++ server/dnxNebMain.c
@@ -974,8 +974,8 @@ static int ehHstCheck(int event_type, void * data)
 // 	hostObj->latency=latency;
 
 	/* Set the command start time */
-	gettimeofday(hstdata->start_time.tv_sec, NULL);
-
+// 	gettimeofday(hstdata->start_time.tv_sec, NULL);
+    hstdata->start_time.tv_sec = time(0);
 	/* set check time for on-demand checks, so they're not incorrectly detected as being orphaned - Luke Ross 5/16/08 */
 	/* NOTE: 06/23/08 EG not sure if there will be side effects to this or not.... */
     extern int scheduled_check;
