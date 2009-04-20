1fec279b26fff099b3e798e19548d84bf0b6afdf
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2009-04-20 04:13:30 +0000

    Fixes.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@516 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxNebMain.c
+++ server/dnxNebMain.c
@@ -1008,6 +1008,8 @@ static int ehHstCheck(int event_type, void * data)
          {
             // Keep trying to get a worker
             dnxDebug(4, "ehHstCheck: Trying %i...", try_count++);
+            wait(5);
+            if(try_count > 3) { break; }
          }
          if(ret == DNX_OK) 
          { 
