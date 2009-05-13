de0a03d68746aa6e3f1798aad24b89fc60030d59
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2009-05-13 08:18:35 +0000

    Fix premature free in dnxRegisterNode.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@628 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxNebMain.c
+++ server/dnxNebMain.c
@@ -984,7 +984,7 @@ static int ehHstCheck(int event_type, void * data)
       return OK;     // tell nagios execute locally
    }
 
-   pNode = (DnxNodeRequest *)xcalloc(sizeof *pNode);  //LEAK
+   pNode = (DnxNodeRequest *)xcalloc(1, sizeof *pNode);  //LEAK
    pNode->flags = dnxGetAffinity(hostObj->name);
    pNode->hn = xstrdup(hostObj->name);
 
