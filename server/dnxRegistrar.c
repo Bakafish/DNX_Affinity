d7a7356e3dc324de453c59330f4ef54a02500aa6
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2009-05-13 05:59:22 +0000

    Reduce leak in  dnxDeleteNodeReq.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@620 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxRegistrar.c
+++ server/dnxRegistrar.c
@@ -171,6 +171,7 @@ static int dnxRegisterNode(iDnxRegistrar * ireg, DnxNodeRequest ** ppMsg)
             dnxErrorString(ret));
       dnxLog("dnxRegisterNode: Unable to enqueue node request: %s.", 
             dnxErrorString(ret));
+      dnxDeleteNodeReq(*ppMsg);
    }
    
 
