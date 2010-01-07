195da281a1fe4245bddc0dc25a7692945ad47acc
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2010-01-07 11:44:11 +0000

    change the job expiration and affinity matching code
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@858 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxRegistrar.c
+++ server/dnxRegistrar.c
@@ -427,12 +427,13 @@ int dnxGetNodeRequest(DnxRegistrar * reg, DnxNodeRequest ** ppNode) {
    }
 
    if((ret = dnxQueueRemove(ireg->rqueue, (void **)ppNode, dnxCompareAffinityNodeReq)) == DNX_QRES_FOUND) {
-      // we found a match
-         dnxDebug(1, "dnxGetNodeRequest: Found Hostnode [%s]:(%qu) with Affinity to dnxClient [%s]:(%qu) Returning(%i).",
-            pNode->hn, pNode->flags, (*(DnxNodeRequest **)ppNode)->hn, (*(DnxNodeRequest **)ppNode)->flags, ret);   
-         // ppNode now points at the dnxClient node , so we need to delete the 
-         // job request at pNode to prevent leaks
-         dnxDeleteNodeReq(pNode); 
+      // make sure we return that we found a match...
+      ret = DNX_OK;
+      dnxDebug(1, "dnxGetNodeRequest: Found Hostnode [%s]:(%qu) with Affinity to dnxClient [%s]:(%qu) Returning(%i).",
+         pNode->hn, pNode->flags, (*(DnxNodeRequest **)ppNode)->hn, (*(DnxNodeRequest **)ppNode)->flags, ret);   
+      // ppNode now points at the dnxClient node , so we need to delete the 
+      // job request at pNode to prevent leaks
+      dnxDeleteNodeReq(pNode);
    } else {
       ret = DNX_ERR_NOTFOUND;
       dnxDebug(1, "dnxGetNodeRequest: didn't find a match. Returning (%i)", ret);
