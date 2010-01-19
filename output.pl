799746b94caa63131fad13b0bd38e2484e77bea6
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2010-01-19 01:42:24 +0000

    Add test script.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@929 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- output.pl
+++ output.pl
@@ -4,15 +4,15 @@ use Data::Dumper;
 
 open (FILE, "</usr/local/dnx/var/log/dnxsrv.log.test");
 
-my %counter;
+my @counter;
 my $types = {
-    ASSIGN => 1,
-    DISPATCH => 2,
-    ACK => 3,
-    COLLECT => 4,
-    RESPONSE => 5,
-    DECLINE => 6,
-    EXPIRE => 7
+    ASSIGN => 0,
+    DISPATCH => 1,
+    ACK => 2,
+    COLLECT => 3,
+    RESPONSE => 4,
+    DECLINE => 5,
+    EXPIRE => 6
     };
 $types = $types, reverse $types;
 
@@ -20,7 +20,9 @@ my $match = qr(.*\ (ASSIGN|DISPATCH|ACK|COLLECT|RESPONSE|DECLINE|EXPIRE):\ Job\ 
 
 while(<FILE>) {
     $_ =~ m|$match|;
-    ${$counter{$2}}[$types->{$1}]++;
+    ${$counter[$2]}[$types->{$1}]++;
 }
+
+
 
 print Dumper \%counter;
