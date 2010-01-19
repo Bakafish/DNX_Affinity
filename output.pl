6a751e20b085e5989331c5bbb4ec65a5ed7f3fa3
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2010-01-19 01:54:03 +0000

    Add test script.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@931 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- output.pl
+++ output.pl
@@ -6,13 +6,13 @@ open (FILE, "</usr/local/dnx/var/log/dnxsrv.log.test");
 
 my @counter;
 my $types = {
-    ASSIGN => 0,
-    DISPATCH => 1,
-    ACK => 2,
-    COLLECT => 3,
-    RESPONSE => 4,
-    DECLINE => 5,
-    EXPIRE => 6
+    ASSIGN => 1,
+    DISPATCH => 2,
+    ACK => 3,
+    COLLECT => 4,
+    RESPONSE => 5,
+    DECLINE => 6,
+    EXPIRE => 7
     };
 $types = $types, reverse $types;
 
@@ -20,9 +20,18 @@ my $match = qr(.*\ (ASSIGN|DISPATCH|ACK|COLLECT|RESPONSE|DECLINE|EXPIRE):\ Job\ 
 
 while(<FILE>) {
     $_ =~ m|$match|;
-    ${$counter[$2]}[$types->{$1}]++;
+    my ($event, $job) = ($1, $2);
+    ${$counter[$job]}[0] = $job;
+    ${$counter[$job]}[$types->{$event}]++;
 }
 
+foreach my $job (@counter) {
+    print "Job: ", shift @{$job};
+    for(my $i=1; $i<8; $i++) {
+        if(defined $job->[$i]) {
+            print " $types->{$i}: $job->[$i] ";
+        }
+    }
+    print "\n";
+}
 
-
-print Dumper \@counter;
