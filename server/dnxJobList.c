b94d87f04636ff3a4bafd7b56afab1369cc58b80
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2010-01-21 10:24:58 +0000

    Fix race condition
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@1007 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxJobList.c
+++ server/dnxJobList.c
@@ -140,7 +140,7 @@ int dnxJobListMarkAckSent(DnxJobList * pJobList, DnxXID * pXid) {
    DNX_PT_MUTEX_LOCK(&ilist->mut);
    if (dnxEqualXIDs(pXid, &ilist->list[current].xid)) {
       if(ilist->list[current].state == DNX_JOB_INPROGRESS) {
-         ilist->list[current].ack = true;
+         ilist->list[current].ack = TRUE;
          dnxAuditJob(&(ilist->list[current]), "CONFIRMED");
          ret = DNX_OK;
       }
