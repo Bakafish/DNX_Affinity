7dd3a9fda3ab0511c78974736b24ef2c19805270
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2010-01-26 11:23:15 +0000

    Track down bad flag assignment
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@1046 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxNode.c
+++ server/dnxNode.c
@@ -27,8 +27,8 @@ DnxNode* dnxNodeListCreateNode(char *address, char *hostname)
             pDnxNode->address = xstrdup(address);
             pDnxNode->hostname = xstrdup(hostname);
             pDnxNode->flags = dnxGetAffinity(pDnxNode->hostname);
-            dnxDebug(4, "dnxNodeListCreateNode: [%s,%s] flags:(%qu) (%qu) (%qu)", 
-            pDnxNode->address, pDnxNode->hostname, pDnxNode->flags, pDnxNode->flags, dnxGetAffinity(pDnxNode->hostname));
+            dnxDebug(4, "dnxNodeListCreateNode: [%s,%s] flags:(%qu) (%qu) (%qu) (%qu)", 
+            pDnxNode->address, pDnxNode->hostname, pDnxNode->flags, dnxGetAffinity(pDnxNode->hostname), dnxGetAffinity(pDnxNode->hostname), pDnxNode->flags);
             
             // Push it behind the head
             pDnxNode->prev = pTopDnxNode;
@@ -45,8 +45,8 @@ DnxNode* dnxNodeListCreateNode(char *address, char *hostname)
         pDnxNode->address = xstrdup(address);
         pDnxNode->hostname = xstrdup(hostname);
         pDnxNode->flags = dnxGetAffinity(pDnxNode->hostname);
-        dnxDebug(4, "dnxNodeListCreateNode: [%s,%s] flags:(%qu) (%qu) (%qu)", 
-        pDnxNode->address, pDnxNode->hostname, pDnxNode->flags, pDnxNode->flags, dnxGetAffinity(pDnxNode->hostname));
+        dnxDebug(4, "dnxNodeListCreateNode: [%s,%s] flags:(%qu) (%qu) (%qu) (%qu)", 
+        pDnxNode->address, pDnxNode->hostname, pDnxNode->flags, dnxGetAffinity(pDnxNode->hostname), dnxGetAffinity(pDnxNode->hostname), pDnxNode->flags);
         pDnxNode->prev = NULL;
         pDnxNode->next = NULL;
     }
