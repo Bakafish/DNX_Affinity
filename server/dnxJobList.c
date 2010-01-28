03293d46e4fcc6b4bb02691e349b58444ef57b70
jason <jason@2be5450d-c085-40c6-83eb-9dc9c13f2dba>
2010-01-28 11:53:43 +0000

    Fix the bad pNode allocation.
    git-svn-id: https://svn.forschooner.net/code/dnx/trunk/dnx@1087 2be5450d-c085-40c6-83eb-9dc9c13f2dba

--- server/dnxJobList.c
+++ server/dnxJobList.c
@@ -333,11 +333,13 @@ int dnxJobListDispatch(DnxJobList * pJobList, DnxNewJob * pJob)
                      
                      // reset the node
                      dnxDeleteNodeReq(ilist->list[current].pNode);
-                     ilist->list[current].pNode->flags = *(dnxGetAffinity(ilist->list[current].host_name));
-                     ilist->list[current].pNode->hn = xstrdup(ilist->list[current].host_name);
-                     ilist->list[current].pNode->addr = NULL;
-                     ilist->list[current].pNode->xid.objSlot = -1;
-                     ilist->list[current].pNode->xid.objSerial = ilist->list[current].xid.objSerial;
+                     DnxNodeRequest * pNode = dnxCreateNodeReq();
+                     pNode->flags = *(dnxGetAffinity(ilist->list[current].host_name));
+                     pNode->hn = xstrdup(ilist->list[current].host_name);
+                     pNode->addr = NULL;
+                     pNode->xid.objSlot = -1;
+                     pNode->xid.objSerial = ilist->list[current].xid.objSerial;
+                     ilist->list[current].pNode = pNode;
                   }
                   break;                  
                } else {
